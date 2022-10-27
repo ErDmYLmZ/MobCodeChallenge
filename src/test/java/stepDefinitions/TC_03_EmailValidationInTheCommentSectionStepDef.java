c7cc13dc57356201d933d19016cecf1508a5a761
ErDmYLmZ <erdemyilmaz4@gmail.com>
2022-10-27 20:20:35 +0200

    gitIgnore was completed

--- src/test/java/stepDefinitions/TC_03_EmailValidationInTheCommentSectionStepDef.java
+++ src/test/java/stepDefinitions/TC_03_EmailValidationInTheCommentSectionStepDef.java
@@ -22,13 +22,12 @@ public class TC_03_EmailValidationInTheCommentSectionStepDef extends JsonPlaceHo
     public void user_fetches_the_comments() {
         response = given().spec(spec).accept(ContentType.JSON).when().get("/{first}");
         Assert.assertEquals("Status code is not 200 ", 200, response.getStatusCode());
-
-
     }
+
     @Then("user validates the emails are in proper format")
     public void user_validates_the_emails_are_in_proper_format() {
        List<String> emails = response.jsonPath().getList("email");
-       var falseList= emails.stream().filter(t-> !EmailValidator.getInstance().isValid(t)).peek(System.out::println).collect(Collectors.toList());
+        var falseList= emails.stream().filter(t-> !EmailValidator.getInstance().isValid(t)).peek(System.out::println).collect(Collectors.toList());
        Assert.assertEquals(0, falseList.size());
 
     }
